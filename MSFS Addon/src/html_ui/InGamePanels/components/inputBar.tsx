991a08c907c2e6eafd8b3647505891a78c835671
Meqolo <iamanubbert@gmail.com>
2023-11-25 11:32:37 +0000

    Introduce eslint

--- MSFS Addon/src/html_ui/InGamePanels/components/inputBar.tsx
+++ MSFS Addon/src/html_ui/InGamePanels/components/inputBar.tsx
@@ -1,81 +1,81 @@
 import {
-    ComponentProps, DisplayComponent, Fragment, FSComponent, NodeReference, VNode
+	ComponentProps, DisplayComponent, FSComponent, NodeReference, VNode
 } from '@microsoft/msfs-sdk';
 
 interface InputBarProps extends ComponentProps {
-    class?: string;
-    id?: string;
-    transformInput?: (input: string) => string;
-    onInput?: (input: string, ref: NodeReference<any>) => void;
-    onEnterKey?: () => void;
-    requireInput?: boolean
+	class?: string;
+	id?: string;
+	transformInput?: (input: string) => string;
+	onInput?: (input: string, ref: NodeReference<HTMLElement>) => void;
+	onEnterKey?: () => void;
+	requireInput?: boolean
 }
 
-const ENTER_KEYCODE = 13
+const ENTER_KEYCODE = 13;
 
 export class InputBar extends DisplayComponent<InputBarProps> {
-    private readonly ref = FSComponent.createRef<any>();
+	private readonly ref = FSComponent.createRef<any>();
 
-    public onAfterRender(node: VNode): void {
-        this.ref.instance.addEventListener("input", () => this.onInputSent())
-        this.ref.instance.addEventListener("keyup", (event: KeyboardEvent) => this.onKeyDown(event))
+	public onAfterRender(): void {
+		this.ref.instance.addEventListener('input', () => this.onInputSent());
+		this.ref.instance.addEventListener('keyup', (event: KeyboardEvent) => this.onKeyDown(event));
 
-        if (this.props.requireInput) {
-            this.setInputError()
-        }
-    }
+		if (this.props.requireInput) {
+			this.setInputError();
+		}
+	}
 
-    private setInputError(remove?: boolean) {
-        if (this.ref.instance.classList.contains("error") !== true && remove !== true) {
-            this.ref.instance.classList.add("error")
-        } else if (this.ref.instance.classList.contains("error") === true && remove === true) {
-            this.ref.instance.classList.remove("error")
-        }
-    }
+	private setInputError(remove?: boolean) {
+		if (this.ref.instance.classList.contains('error') !== true && remove !== true) {
+			this.ref.instance.classList.add('error');
+		} else if (this.ref.instance.classList.contains('error') === true && remove === true) {
+			this.ref.instance.classList.remove('error');
+		}
+	}
 
-    private onKeyDown(event: KeyboardEvent): void {
-        // My keyboard is returning the enter key as being 0xFFFF for whatever reason
-        if ((event.keyCode == ENTER_KEYCODE || event.keyCode == 0xFFFFFFFF) && this.props.onEnterKey) {
-            this.props.onEnterKey()
-        }
-    }
+	private onKeyDown(event: KeyboardEvent): void {
+		// My keyboard is returning the enter key as being 0xFFFF for whatever reason
+		if ((event.keyCode == ENTER_KEYCODE || event.keyCode == 0xFFFFFFFF) && this.props.onEnterKey) {
+			this.props.onEnterKey();
+		}
+	}
 
-    private onInputSent() {
-        let input = this.ref.instance._valueStr;
-        if (input !== undefined) {
-            if (this.props.transformInput) {
-                input = this.props.transformInput(input)
-                this.ref.instance.setValue(input)
-            }
-            if (this.props.onInput) {
-                this.props.onInput(input, this.ref)
-            }
-            if (this.props.requireInput) {
-                if (input == "") {
-                    this.setInputError()
-                } else {
-                    this.setInputError(true)
-                }
-            }
-        }
-    }
+	private onInputSent() {
+		let input = this.ref.instance._valueStr;
+		if (input !== undefined) {
+			if (this.props.transformInput) {
+				input = this.props.transformInput(input);
+				this.ref.instance.setValue(input);
+			}
+			if (this.props.onInput) {
+				this.props.onInput(input, this.ref);
+			}
+			if (this.props.requireInput) {
+				if (input == '') {
+					this.setInputError();
+				} else {
+					this.setInputError(true);
+				}
+			}
+		}
+	}
 
-    public setInputText(input: string) {
-        this.ref.instance.setValue(input)
-        this.onInputSent()
-    }
+	public setInputText(input: string) {
+		this.ref.instance.setValue(input);
+		this.onInputSent();
+	}
 
-    public getInputBar(): NodeReference<HTMLElement> {
-        return this.ref
-    }
+	public getInputBar(): NodeReference<HTMLElement> {
+		return this.ref;
+	}
 
-    public unFocus(): void {
-        this.getInputBar().instance.blur()
-    }
+	public unFocus(): void {
+		this.getInputBar().instance.blur();
+	}
 
-    public render(): VNode {
-        return (
-            <ui-input ref={this.ref} class={this.props.class} id={this.props.id} type="text" no-tooltip no-key-navigation not-pad-interactive idevent="0" />
-        );
-    }
+	public render(): VNode {
+		return (
+			<ui-input ref={this.ref} class={this.props.class} id={this.props.id} type="text" no-tooltip no-key-navigation not-pad-interactive idevent="0" />
+		);
+	}
 }
--- MSFS Addon/src/html_ui/Pages/ToolBar/vPilotPersistent/RadioPanel.tsx
+++ MSFS Addon/src/html_ui/Pages/ToolBar/vPilotPersistent/RadioPanel.tsx
@@ -1,192 +1,191 @@
 import {
-    ComponentProps, DisplayComponent, EventBus, FSComponent, Subject, VNode
+	ComponentProps, DisplayComponent, EventBus, FSComponent, Subject, VNode
 } from '@microsoft/msfs-sdk';
 
 import { InputBar } from '../../../InGamePanels/components/inputBar';
 import { VirtualScroll } from '../../../InGamePanels/components/virtualScroll';
 import { BackendEvents, FrontendEvents } from '../../../InGamePanels/vPEBackend';
-import { checkSimVarLoaded } from './Utilites';
 
 export interface MessageProps {
-    callsign: string;
-    currentCallsign?: string
-    frequencies: string[];
-    broadcast: boolean
-    message: string;
+	callsign: string;
+	currentCallsign?: string
+	frequencies: string[];
+	broadcast: boolean
+	message: string;
 }
 
 class RadioMessage extends DisplayComponent<MessageProps> {
-    private readonly timeStamp = new Date().toUTCString().slice(17, 25)
-    private message = this.props.message
+	private readonly timeStamp = new Date().toUTCString().slice(17, 25);
+	private message = this.props.message;
 
-    private determineTextColour() {
-        if (this.props.currentCallsign && this.props.message.includes(this.props.currentCallsign)) {
-            return `rgb(255, 255, 255)`
-        } else if (this.props.broadcast) {
-            return `rgb(250, 250, 100)`
-        } else {
-            return `rgb(200,200,200)`
-        }
-    }
+	private determineTextColour() {
+		if (this.props.currentCallsign && this.props.message.includes(this.props.currentCallsign)) {
+			return 'rgb(255, 255, 255)';
+		} else if (this.props.broadcast) {
+			return 'rgb(250, 250, 100)';
+		} else {
+			return 'rgb(200,200,200)';
+		}
+	}
 
-    public onAfterRender() {
-        if (this.props.currentCallsign) {
-            this.message = this.message.replace(this.props.currentCallsign, `<strong>${this.props.currentCallsign}</strong>`)
-        }
-    }
+	public onAfterRender() {
+		if (this.props.currentCallsign) {
+			this.message = this.message.replace(this.props.currentCallsign, `<strong>${this.props.currentCallsign}</strong>`);
+		}
+	}
 
-    public render() {
-        return (
-            <tr>
-                <td class="w-[1%] whitespace-nowrap px-2 align-top" style={`color: ${this.determineTextColour()}`}>[<span class="font-bold">{this.props.callsign}</span>]</td>
-                <td class="overflow-hidden align-center" style={`color: ${this.determineTextColour()}`}>{this.message}</td>
-                <td class="w-[1%] whitespace-nowrap px-2 align-bottom"><span class="text-sm align-bottom"><code class="[color:rgb(210,210,210)]">{this.timeStamp}Z</code></span></td>
-            </tr>
-        )
-    }
+	public render() {
+		return (
+			<tr>
+				<td class="w-[1%] whitespace-nowrap px-2 align-top" style={`color: ${this.determineTextColour()}`}>[<span class="font-bold">{this.props.callsign}</span>]</td>
+				<td class="overflow-hidden align-center" style={`color: ${this.determineTextColour()}`}>{this.message}</td>
+				<td class="w-[1%] whitespace-nowrap px-2 align-bottom"><span class="text-sm align-bottom"><code class="[color:rgb(210,210,210)]">{this.timeStamp}Z</code></span></td>
+			</tr>
+		);
+	}
 }
 
 interface RadioSystemMessageProps {
-    frequency: string
+	frequency: string
 }
 class RadioSystemMessage extends DisplayComponent<RadioSystemMessageProps> {
-    private readonly timeStamp = new Date().toUTCString().slice(17, 25)
+	private readonly timeStamp = new Date().toUTCString().slice(17, 25);
 
-    public render() {
-        return (
-            <tr>
-                <td class="w-[1%] whitespace-nowrap px-2 align-top"></td>
-                <td class="overflow-hidden align-center text-center py-2" style="color: rgb(30,200,250);">FREQUENCY CHANGED TO <strong>1{this.props.frequency.slice(0, 2)}.{this.props.frequency.slice(2)}</strong></td>
-                <td class="w-[1%] whitespace-nowrap px-2 align-bottom"><span class="text-sm align-bottom"><code class="[color:rgb(210,210,210)]">{this.timeStamp}Z</code></span></td>
-            </tr >
-        )
-    }
+	public render() {
+		return (
+			<tr>
+				<td class="w-[1%] whitespace-nowrap px-2 align-top"></td>
+				<td class="overflow-hidden align-center text-center py-2" style="color: rgb(30,200,250);">FREQUENCY CHANGED TO <strong>1{this.props.frequency.slice(0, 2)}.{this.props.frequency.slice(2)}</strong></td>
+				<td class="w-[1%] whitespace-nowrap px-2 align-bottom"><span class="text-sm align-bottom"><code class="[color:rgb(210,210,210)]">{this.timeStamp}Z</code></span></td>
+			</tr >
+		);
+	}
 }
 
 interface RadioPanelProps extends ComponentProps {
-    bus: EventBus
+	bus: EventBus
 }
 export class RadioPanel extends DisplayComponent<RadioPanelProps> {
-    private readonly publisher = this.props.bus.getPublisher<FrontendEvents>();
-    private readonly subscriber = this.props.bus.getSubscriber<BackendEvents>();
-    private readonly buttonRef = FSComponent.createRef<HTMLElement>()
-    private readonly inputBarRef = FSComponent.createRef<InputBar>()
-    private readonly scrollRef = FSComponent.createRef<VirtualScroll>()
-    private readonly mainRef = FSComponent.createRef<HTMLDivElement>()
-    private readonly messageContainerRef = FSComponent.createRef<HTMLTableElement>()
-    private readonly currentCallsign = Subject.create<string | undefined>(undefined);
-    private currentFrequency?: string;
-    private currentMessage: string = '';
-    public readonly radioOpen = Subject.create<boolean>(false)
+	private readonly publisher = this.props.bus.getPublisher<FrontendEvents>();
+	private readonly subscriber = this.props.bus.getSubscriber<BackendEvents>();
+	private readonly buttonRef = FSComponent.createRef<HTMLElement>();
+	private readonly inputBarRef = FSComponent.createRef<InputBar>();
+	private readonly scrollRef = FSComponent.createRef<VirtualScroll>();
+	private readonly mainRef = FSComponent.createRef<HTMLDivElement>();
+	private readonly messageContainerRef = FSComponent.createRef<HTMLTableElement>();
+	private readonly currentCallsign = Subject.create<string | undefined>(undefined);
+	private currentFrequency?: string;
+	private currentMessage: string = '';
+	public readonly radioOpen = Subject.create<boolean>(false);
 
-    private renderMessage(callsign: string, message: string, broadcast: boolean, frequencies: string[] = ["99999"]) {
-        if (frequencies[0] != this.currentFrequency) {
-            this.currentFrequency = frequencies[0]
-            FSComponent.render(<RadioSystemMessage frequency={this.currentFrequency} />, this.messageContainerRef.instance)
-        }
+	private renderMessage(callsign: string, message: string, broadcast: boolean, frequencies: string[] = ['99999']) {
+		if (frequencies[0] != this.currentFrequency) {
+			this.currentFrequency = frequencies[0];
+			FSComponent.render(<RadioSystemMessage frequency={this.currentFrequency} />, this.messageContainerRef.instance);
+		}
 
-        FSComponent.render(<RadioMessage broadcast={broadcast} callsign={callsign} message={message} frequencies={frequencies} currentCallsign={this.currentCallsign.get()} />, this.messageContainerRef.instance)
-    }
+		FSComponent.render(<RadioMessage broadcast={broadcast} callsign={callsign} message={message} frequencies={frequencies} currentCallsign={this.currentCallsign.get()} />, this.messageContainerRef.instance);
+	}
 
-    public onAfterRender(node: VNode): void {
-        let inputParent = this.inputBarRef.instance.getInputBar().instance
-        this.buttonRef.instance.addEventListener('click', () => this.sendMessage())
+	public onAfterRender(): void {
+		const inputParent = this.inputBarRef.instance.getInputBar().instance;
+		this.buttonRef.instance.addEventListener('click', () => this.sendMessage());
 
-        const checkParentLoaded: Promise<HTMLDivElement> = new Promise(resolve => {
-            const interval = setInterval(() => {
-                let inputDiv = inputParent.querySelector(".default-input")
-                if (inputDiv !== null) {
-                    clearInterval(interval);
-                    resolve(inputDiv as HTMLDivElement);
-                }
-            });
+		const checkParentLoaded: Promise<HTMLDivElement> = new Promise(resolve => {
+			const interval = setInterval(() => {
+				const inputDiv = inputParent.querySelector('.default-input');
+				if (inputDiv !== null) {
+					clearInterval(interval);
+					resolve(inputDiv as HTMLDivElement);
+				}
+			});
 
-            this.subscriber.on('newMessage').handle((message) => this.renderMessage(message.callsign, message.message, message.broadcast, message.frequencies))
-            this.subscriber.on('deleteMessage').handle((message) => {
-                for (let i = this.messageContainerRef.instance.childNodes.length - 1; i >= 0; i--) {
-                    let node = this.messageContainerRef.instance.childNodes[i]
-                    let includesMsg = node.textContent?.includes(message)
-                    if (includesMsg) {
-                        node.remove()
-                        return
-                    }
-                }
-            })
-            this.subscriber.on('networkCallsign').handle((callsign) => this.currentCallsign.set(callsign));
-        });
+			this.subscriber.on('newMessage').handle((message) => this.renderMessage(message.callsign, message.message, message.broadcast, message.frequencies));
+			this.subscriber.on('deleteMessage').handle((message) => {
+				for (let i = this.messageContainerRef.instance.childNodes.length - 1; i >= 0; i--) {
+					const node = this.messageContainerRef.instance.childNodes[i];
+					const includesMsg = node.textContent?.includes(message);
+					if (includesMsg) {
+						node.remove();
+						return;
+					}
+				}
+			});
+			this.subscriber.on('networkCallsign').handle((callsign) => this.currentCallsign.set(callsign));
+		});
 
-        checkParentLoaded.then((inputDiv) => { inputDiv.style.height = "41px" })
-        this.scrollRef.instance.setViewportHeight(this.mainRef.instance.getBoundingClientRect().height - 49)
+		checkParentLoaded.then((inputDiv) => { inputDiv.style.height = '41px'; });
+		this.scrollRef.instance.setViewportHeight(this.mainRef.instance.getBoundingClientRect().height - 49);
 
-        this.radioOpen.sub(open => open ? this.openRadio() : this.closeRadio())
+		this.radioOpen.sub(open => open ? this.openRadio() : this.closeRadio());
 
-        const observerConfig: MutationObserverInit = { childList: true };
-        const mutationObserver = new MutationObserver((mutations: MutationRecord[]) => {
-            mutations.forEach((mutation) => {
-                if (mutation.type === 'childList') {
-                    // Child element added, scroll to bottom
-                    this.scrollRef.instance.scrollToBottom();
-                }
-            })
-        })
+		const observerConfig: MutationObserverInit = { childList: true };
+		const mutationObserver = new MutationObserver((mutations: MutationRecord[]) => {
+			mutations.forEach((mutation) => {
+				if (mutation.type === 'childList') {
+					// Child element added, scroll to bottom
+					this.scrollRef.instance.scrollToBottom();
+				}
+			});
+		});
 
-        mutationObserver.observe(this.messageContainerRef.instance, observerConfig);
-    }
+		mutationObserver.observe(this.messageContainerRef.instance, observerConfig);
+	}
 
-    private onTextbarInput(input: string) {
-        this.currentMessage = input
-    }
+	private onTextbarInput(input: string) {
+		this.currentMessage = input;
+	}
 
-    private sendMessage() {
-        if (this.currentCallsign.get() && this.currentMessage && this.currentMessage.length > 0) {
-            this.publisher.pub('sendMessage', this.currentMessage)
-        }
-        this.currentMessage = ''
-        this.inputBarRef.instance.setInputText('')
-    }
+	private sendMessage() {
+		if (this.currentCallsign.get() && this.currentMessage && this.currentMessage.length > 0) {
+			this.publisher.pub('sendMessage', this.currentMessage);
+		}
+		this.currentMessage = '';
+		this.inputBarRef.instance.setInputText('');
+	}
 
-    private openRadio() {
-        this.mainRef.instance.style.transform = `translateY(0px)`
-        this.mainRef.instance.style.maxHeight = '400px'
-        setTimeout(() => {
-            this.scrollRef.instance.scrollToBottom()
-        }, 500);
-    }
+	private openRadio() {
+		this.mainRef.instance.style.transform = 'translateY(0px)';
+		this.mainRef.instance.style.maxHeight = '400px';
+		setTimeout(() => {
+			this.scrollRef.instance.scrollToBottom();
+		}, 500);
+	}
 
-    private closeRadio() {
-        this.inputBarRef.instance.unFocus()
-        this.mainRef.instance.style.maxHeight = '52px'
-        this.mainRef.instance.style.transform = `translateY(-350px)`
-    }
+	private closeRadio() {
+		this.inputBarRef.instance.unFocus();
+		this.mainRef.instance.style.maxHeight = '52px';
+		this.mainRef.instance.style.transform = 'translateY(-350px)';
+	}
 
-    public render(): VNode | null {
-        return (
-            <div>
-                <link rel="import" href="/templates/uiInput/uiInput.html" />
-                <link rel="import" href="/templates/InputBar/InputBar.html" />
+	public render(): VNode | null {
+		return (
+			<div>
+				<link rel="import" href="/templates/uiInput/uiInput.html" />
+				<link rel="import" href="/templates/InputBar/InputBar.html" />
 
-                <div ref={this.mainRef} class="bg-[var(--backgroundColorContrastedPanel)] p-1 m-2 overflow-hidden" style="transform: translateY(-350px); max-height: 350px; transition: 0.5s ease-in-out;">
-                    <div class="bg-[var(--backgroundColorPanel)] flex items-stretch justify-between" id="topbar">
-                        <div class="font-bold pl-2 py-1 text-xl">[VPILOT EXTENDER] RADIO</div>
-                        <icon-button class="Reduce nodrag condensed-interactive m-1" icon-tooltip="TT:GAME.PANEL_TOOLTIP_MINIMIZE" data-url="/icons/reduce.svg" created="true" data-input-group="ICON-BUTTON" tabindex="1" guid="GUID_b7d51b5d-4def-54cf-f4cb-7060e98eac00" />
-                    </div>
+				<div ref={this.mainRef} class="bg-[var(--backgroundColorContrastedPanel)] p-1 m-2 overflow-hidden" style="transform: translateY(-350px); max-height: 350px; transition: 0.5s ease-in-out;">
+					<div class="bg-[var(--backgroundColorPanel)] flex items-stretch justify-between" id="topbar">
+						<div class="font-bold pl-2 py-1 text-xl">[VPILOT EXTENDER] RADIO</div>
+						<icon-button class="Reduce nodrag condensed-interactive m-1" icon-tooltip="TT:GAME.PANEL_TOOLTIP_MINIMIZE" data-url="/icons/reduce.svg" created="true" data-input-group="ICON-BUTTON" tabindex="1" guid="GUID_b7d51b5d-4def-54cf-f4cb-7060e98eac00" />
+					</div>
 
-                    <div class="pt-2 pr-[-0.5rem] h-[224px] overflow-hidden">
-                        <VirtualScroll ref={this.scrollRef}>
-                            <table class="w-full min-w-full [word-break:break-all]" ref={this.messageContainerRef} />
-                        </VirtualScroll>
-                    </div>
+					<div class="pt-2 pr-[-0.5rem] h-[224px] overflow-hidden">
+						<VirtualScroll ref={this.scrollRef}>
+							<table class="w-full min-w-full [word-break:break-all]" ref={this.messageContainerRef} />
+						</VirtualScroll>
+					</div>
 
-                    <div class="grid grid-cols-5 pt-1">
-                        <div class="col-span-4">
-                            <InputBar onInput={this.onTextbarInput.bind(this)} onEnterKey={this.sendMessage.bind(this)} ref={this.inputBarRef} class="h-auto" />
-                        </div>
-                        <div>
-                            <new-push-button ref={this.buttonRef} class="w-auto ml-2 text-center" title="Send" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+					<div class="grid grid-cols-5 pt-1">
+						<div class="col-span-4">
+							<InputBar onInput={this.onTextbarInput.bind(this)} onEnterKey={this.sendMessage.bind(this)} ref={this.inputBarRef} class="h-auto" />
+						</div>
+						<div>
+							<new-push-button ref={this.buttonRef} class="w-auto ml-2 text-center" title="Send" />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
